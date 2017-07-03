731363a241deb22127390dbe86fda234f82b95fb
sliv <sliv.mbp@gmail.com>
2017-07-04 06:41:35 +0800

    fix: use done check to detect completion instead of the return result

--- src/index.ts
+++ src/index.ts
@@ -19,12 +19,24 @@ export default function match<A = any> (items: ([any, ((p: any) => A) | A])[], c
   }
 
   const pairs = items.map(normalize);
-  return function (target) {
+  return function (target?) {
+    let done = false;
+
     function reducer (pr, [cond, ret]) {
-      if (pr) return pr;
-      return cond(target) ? ret(target) : pr;
+      if (done) return pr;
+      if (cond(target)) {
+        done = true;
+        return ret(target);
+      }
+      return pr;
     }
 
     return pairs.reduce(reducer, undefined);
   };
 }
+
+export const matchArg = (item?) =>
+  <A> (condPairs: ([any, ((p: any) => A) | A])[], cmpFn = isShallowEqual) =>
+    match(condPairs, cmpFn)(item);
+
+export const switcher = matchArg();
